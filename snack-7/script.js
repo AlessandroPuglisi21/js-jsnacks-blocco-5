e419e335cb68bf9aabaabe086c358af55d663b4b
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-30 15:02:07 +0100

    Update:snack-7 find methods()

--- snack-7/script.js
+++ snack-7/script.js
@@ -22,4 +22,8 @@ const students = [
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
 
+const studentSearch = students.find(student => student.id === 2);
+
+console.log(studentSearch)
+
 console.log(students[1])
