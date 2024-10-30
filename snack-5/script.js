95dfc7d932a0c851362c0a0513cc0f16c9b14baf
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-30 15:46:02 +0100

    Update: Snack-5 find.method()

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,15 +1,27 @@
-const nums = [2, 8, 4, 7, 12, 87];
+const nums = [2, 8, 4, 7, 12, 87,3,14,15,12,78,55];
 
 numeriPari = [];
 numeriDispari = []
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
 
-nums.forEach((num) =>{
+// nums.forEach((num) =>{
+//     if(num % 2 === 0){
+//         numeriPari.push(num)
+//     }else
+//         numeriDispari.push(num)
+// })
+// console.log(numeriPari)
+// console.log(numeriDispari)
+
+//! METODO FIND 
+
+numberType = nums.find((num) =>{
     if(num % 2 === 0){
-        numeriPari.push(num)
-    }else
-        numeriDispari.push(num)
+        numeriPari.push(`${num} é Pari`)
+    } else
+    numeriDispari.push(`${num} é dispari`)
+    
 })
-console.log(numeriPari)
-console.log(numeriDispari)
+
+console.log(numeriPari, numeriDispari)
