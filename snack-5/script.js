1f757a80570919dca4100b3644ed4751a29136e1
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-10-30 14:40:24 +0100

    UPDATE: Snack-5 added Dispari

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,11 +1,15 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 numeriPari = [];
+numeriDispari = []
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
 
 nums.forEach((num) =>{
-    if(num % 2 === 0)
+    if(num % 2 === 0){
         numeriPari.push(num)
+    }else
+        numeriDispari.push(num)
 })
 console.log(numeriPari)
+console.log(numeriDispari)
